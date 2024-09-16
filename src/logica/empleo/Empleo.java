be3160586deb1657417abe92217126419af0cb65
Manuel Montero <87241159+ManuelMC21@users.noreply.github.com>
2024-09-17 00:28:33 +0200

    Mas Pruebas y Cosas arregaldas para mejorar el rendimiento y control del usuario **Candela**

--- src/logica/empleo/Empleo.java
+++ src/logica/empleo/Empleo.java
@@ -4,21 +4,69 @@ import logica.empresa.Empresa;
 import logica.enums.Rama;
 
 public class Empleo {
-	
+
 	private String ID;
 	private String rama;
 	private Double salario;
 	private Empresa empOfertante;
 	private Rama ramaEmp;
-	
+
 	public Empleo( String id, String rama, double sal, Empresa empO, Rama ramaEmp){
 		this.setID(id);
 		this.setRama(rama);
 		this.setSalario(sal);
 		this.setEmpOfertante(empO);	
-		this.ramaEmp = ramaEmp;
+		this.setRamaEmp(ramaEmp);
 	}
 	
+	private void validarRamaEmp(Rama r){
+		if (r == null){
+			throw new IllegalArgumentException("Rama de empleo vacia");
+		}
+	}
+	
+	private void setRamaEmp(Rama r){
+		try {
+			validarRamaEmp(r);
+			this.ramaEmp = r;
+		} catch (IllegalArgumentException e) {
+			throw e;
+		}
+	}
+
+	private void validarSalario(Double sal){
+		if(sal <= 0){
+			throw new IllegalArgumentException("Salario no valido");
+		}
+	}
+	
+	private void validarOfertante(Empresa emp){
+		if(emp == null){
+			throw new IllegalArgumentException("Empresa ofertante null");
+		}
+	}
+
+	public static void validar(String text){
+
+		if(text.trim().length() == 0){ 
+			throw new IllegalArgumentException("Hay un campo vacio");
+		}
+
+		for (int i = 0; i < text.length(); i++){
+			if (text.charAt(i) == ' ')
+				if (i == 0){
+					if(text.charAt(i+1) == ' ' || text.charAt(i) == ' ')
+						throw new IllegalArgumentException("Los campos rellenables no deben comenzar con espacios");
+				}
+				else if(i+1 < text.length()){
+					if(text.charAt(i+1) == ' ' || text.charAt(i-1) == ' ')
+						throw new IllegalArgumentException("Espacios Juntos");
+				}
+				else if(text.charAt(text.length()-1) == ' ')
+					throw new IllegalArgumentException("Los campos rellenables no deben terminar en espacio");
+		}
+	}
+
 	public Rama getRamaEmp(){
 		return this.ramaEmp;
 	}
@@ -28,7 +76,12 @@ public class Empleo {
 	}
 
 	public void setID(String id2) {
-		ID = id2;
+		try {
+			validar(id2);
+			this.ID = id2;
+		} catch (IllegalArgumentException e) {
+			throw e;
+		}
 	}
 
 	public String getRama() {
@@ -36,7 +89,12 @@ public class Empleo {
 	}
 
 	public void setRama(String rama) {
-		this.rama = rama;
+		try {
+			validar(rama);
+			this.rama = rama;
+		} catch (IllegalArgumentException e) {
+			throw e;
+		}
 	}
 
 	public Double getSalario() {
@@ -44,7 +102,12 @@ public class Empleo {
 	}
 
 	public void setSalario(Double salario) {
-		this.salario = salario;
+		try {
+			validarSalario(salario);
+			this.salario = salario;
+		} catch (IllegalArgumentException e) {
+			throw e;
+		}
 	}
 
 	public Empresa getEmpOfertante() {
@@ -52,8 +115,13 @@ public class Empleo {
 	}
 
 	public void setEmpOfertante(Empresa empO) {
-		this.empOfertante = empO;
+		try {
+			validarOfertante(empO);
+			this.empOfertante = empO;
+		} catch (IllegalArgumentException e) {
+			throw e;
+		}
 	}
-	
+
 
 }
